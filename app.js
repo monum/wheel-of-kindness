e379caa0fc93d3fc761fbafd00e9159534994f56
Michael Lawrence Evans <mlevans@users.noreply.github.com>
2020-04-06 18:52:28 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return 1+Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+(function() {
+  const wheel = document.querySelector('#wheel');
+  const startButton = document.querySelector('#button');
+  var deg = 0;
+
+  startButton.addEventListener('click', () => {
+    d3.select('#button').html("Spinning!");
+    startButton.style.pointerEvents = 'none';
+    deg = -1*getRandomInt(720,2880);
+    wheel.style.transition = 'all 4s ease-out';
+    wheel.style.transform = `rotate(${deg}deg)`;
+  });
+
+  wheel.addEventListener('transitionend', () => {
+    startButton.style.pointerEvents = 'auto';
+    wheel.style.transition = 'none';
+    var actualDeg = deg % 360;
+    var piece = Math.ceil(-1*(actualDeg)/45);
+    console.log(piece);
+    d3.select('#button').html("You spun " + piece +"!<br/><br/>Click here to try again!");
+    wheel.style.transform = `rotate(${actualDeg}deg)`;
+  });
+})();
