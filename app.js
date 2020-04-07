f1400856f9b906efbb1a0d7eb05d638c0d09e09f
jcyoungblood <jaclyn.youngblood@gmail.com>
2020-04-07 16:32:50 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var pieces = [
-  "Write a letter to a veteran. To volunteer to be a penpal, please email <a href mailto:vetvolunteer@boston.gov>vetvolunteer@boston.gov</a>.",
-  "p2",
-  "p3",
-  "p4",
-  "p5",
-  "p6",
-  "p7",
-  "p8"
+  "Write a letter to a veteran! To volunteer to be a penpal, please email <a href = "mailto:vetvolunteer@boston.gov">vetvolunteer@boston.gov</a>.",
+  "Sew a face covering. Details via <a href="bostonareamaskinitiative.com",bostonareamaskinitiative.com</a>.",
+  "Call an older adult. Say hello to someone you know via phone or video chat.",
+  "Thank a front-line service worker! When you get groceries, or see someone who works in health care, take a moment to share gratitude.",
+  "Read a book to our BPS scholars. Post the video on Twitter with the hashtag #GoodnightScholarsBPS.",
+  "Organize a virtual block party with your neighbors! Post the playlist link on Twitter with the hashtag #BOSdanceparty",
+  "Share your #IStayHomeFor story. Post a photo of someone you care about use the hashtags #BosStayHome and #IStayHomeFor.",
+  "Are you stronger than BPS Superintendent Dr. Cassellius? Take a video of yourself doing 10 pushups and post online with hashtag #See10Do10Challenge."
 ];
 
 function getRandomInt(min, max) {
@@ -18,14 +18,14 @@ function getRandomInt(min, max) {
 (function() {
   var wheel = document.querySelector('#wheel');
   var startButton = document.querySelector('#button');
-  
+
   var deg = 0;
 
   startButton.addEventListener('click', () => {
     d3.select('#button').html("Spinning!");
     startButton.style.pointerEvents = 'none';
     deg = -1*getRandomInt(720,2880);
-    
+
     if (!(deg % 2)){
       deg+=1;
     }
