dd936eb22ef4fc9bb52647d2cce23790c80ef55d
jcyoungblood <jaclyn.youngblood@gmail.com>
2020-04-07 20:47:33 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var pieces = [
-  "Write a letter to a veteran! To volunteer, email <a href = 'mailto:vetvolunteer@boston.gov'>vetvolunteer@boston.gov</a>.",
-  "Sew a face covering. Details via <a href='bostonareamaskinitiative.com',bostonareamaskinitiative.com</a>.",
-  "Call an older adult. Say hello to someone you know via phone or video chat.",
-  "Thank a front-line service worker! Take a moment to share gratitude however you feel comfortable.",
-  "Read a book to our BPS scholars. Post your video on Twitter with the hashtag #GoodnightScholarsBPS.",
-  "Organize a virtual block party! Post the playlist on Twitter with the hashtag #BOSdanceparty.",
-  "Share your #IStayHomeFor story. Post a photo of someone you care about using the hashtags #BosStayHome and #IStayHomeFor.",
-  "Challenge BPS Superintendent Dr. Cassellius to 10 pushups! Post a video of yourself doing 10 pushups with the hashtag #See10Do10Challenge."
+  "<b>Write a letter to a veteran!</b> <br>To volunteer, email <a href = 'mailto:vetvolunteer@boston.gov'>vetvolunteer@boston.gov</a>.",
+  "<b>Sew a face covering.</b> <br>Details via <a href='bostonareamaskinitiative.com',bostonareamaskinitiative.com</a>.",
+  "<b>Call an older adult.</b> <br>Say hello to someone you know via phone or video chat.",
+  "<b>Thank a front-line service worker!</b> <br>Take a moment to share gratitude however you feel comfortable.",
+  "<b>Read a book to our BPS scholars.</b> <br>Post your video on Twitter with the hashtag #GoodnightScholarsBPS.",
+  "<b>Organize a virtual block party!</b> <br>Post the playlist on Twitter with the hashtag #BOSdanceparty.",
+  "<b>Share your #IStayHomeFor story.</b> <br>Post a photo of someone you care about using the hashtags #BosStayHome and #IStayHomeFor.",
+  "<b>Challenge BPS Superintendent Dr.</b> <br>Cassellius to 10 pushups! Post a video of yourself doing 10 pushups with the hashtag #See10Do10Challenge."
 ];
 
 function getRandomInt(min, max) {
@@ -41,7 +41,7 @@ function getRandomInt(min, max) {
     var piece = Math.ceil(-1*(actualDeg)/45);
     console.log(actualDeg, piece);
     d3.select('#result').html(pieces[piece-1]);
-    d3.select('#button').html("Click here for another act of kindness!");
+    d3.select('#button').html("Click for another act of kindness!");
     wheel.style.transform = `rotate(${actualDeg}deg)`;
   });
 })();
